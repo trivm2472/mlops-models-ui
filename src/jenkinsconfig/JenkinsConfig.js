708cb9078cadd59e5d35bca524756b8c3d163988
trivm2472@gmail.com <trivm2472@gmail.com>
2023-05-28 12:46:19 +0700

    .

--- src/jenkinsconfig/JenkinsConfig.js
+++ src/jenkinsconfig/JenkinsConfig.js
@@ -1,5 +1,5 @@
 const JenkinsConfig = {
-  jenkinsURL: 'https://ffc1-125-235-239-221.ngrok-free.app/',
+  jenkinsURL: 'https://jenkins.mlopshcmut.ngrok.app/login?from=%2F',
   job: 'MLOPS_BUILD_AND_DEPLOY_BACKEND',
   job2: 'MLOPS_DEPLOY'
   // job: 'BUILD_BACKEND_IMAGE_MLOPS'
