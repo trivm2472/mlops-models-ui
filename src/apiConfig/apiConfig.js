cafdb95489fc181a9e33827aa859f290952e8a52
trivm2472@gmail.com <trivm2472@gmail.com>
2023-06-07 17:31:07 +0700

    .

--- src/apiConfig/apiConfig.js
+++ src/apiConfig/apiConfig.js
@@ -1,11 +1,12 @@
-// const apiConfig = {
-//   vercelURL: 'https://mlops-models-server-1gp6.vercel.app',
-// }
-
 const apiConfig = {
-  vercelURL: 'http://localhost:4000',
+  vercelURL: 'https://mlops-models-server-1gp6.vercel.app',
   trainStatus: 'https://jenkins.mlopshcmut.ngrok.app/job/MLOPS_TRAINING_MODEL/'
 }
+
+// const apiConfig = {
+//   vercelURL: 'http://localhost:4000',
+//   trainStatus: 'https://jenkins.mlopshcmut.ngrok.app/job/MLOPS_TRAINING_MODEL/'
+// }
 
 
 export default apiConfig;
