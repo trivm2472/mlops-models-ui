229557d991e76201cc73bef6281947321c9cc0a8
trivm2472@gmail.com <trivm2472@gmail.com>
2023-06-12 14:39:33 +0700

    .

--- src/components/TrainModel/TrainModel.js
+++ src/components/TrainModel/TrainModel.js
@@ -62,7 +62,7 @@ export default function TrainModel() {
             `${JenkinsConfig.jenkinsURL}/job/${JenkinsConfig.trainJob}/job/main/buildWithParameters?MODEL_NAME=${modelName}&DATA_URL=${dataUrl}&IMG=${img}&BATCH=${batch}&EPOCH=${epoch}&WEIGHT=${weight}`,
             {}
           );
-          
+
           const apiResponse = await axios.get(`${apiConfig.vercelURL}/train/${modelName}`)
           setIsOpen(!isOpen);
           alert('Build request has been sent');
@@ -328,7 +328,7 @@ export default function TrainModel() {
               marginBottom: 18,
             }}
           >
-            The weight to start traing from. Example yolov5l.pt
+            The weight to start training from. Example yolov5l.pt
           </div>
           <input
             type="text"
